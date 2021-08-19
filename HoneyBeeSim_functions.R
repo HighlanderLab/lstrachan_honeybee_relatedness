f3ea71271fdc65c6814d05fd2cc3b17408c9c1d4
LStrachan <80959752+LStrachan@users.noreply.github.com>
2021-08-19 15:17:03 +0100

    Update HoneyBeeSim_functions.R
    Identify which functions need to be created

--- HoneyBeeSim_functions.R
+++ HoneyBeeSim_functions.R
@@ -1,1 +1,19 @@
 #For function storage/ developement 
+
+createColony = function() {} # remember to add fathers and id, we will likely need queen age too - but that should go into colony$queen$misc slot!
+
+createSwarm = function() {} 
+# no new drones from existing queen
+# little honey
+# younger queens swarm less
+# build-up after swarm depends on season - should this be an extra function / or will this be in country scripts?
+
+createDCA = function() {}
+
+supersedure = function () {} # how do we make this one into a verb?
+
+beeCross = function() {}
+
+splitColony = function() {}
+
+# setPheno = function() {} # keep this one commented for now, we need some object-oriented magic for this to work on our colony and not to clash with AlphaSimR:::setPheno()
