875385e458a14143905fcc685da3a2ada6c97a19
LStrachan <80959752+LStrachan@users.noreply.github.com>
2021-08-20 10:07:10 +0100

    Update HoneyBeeSim_functions.R

--- HoneyBeeSim_functions.R
+++ HoneyBeeSim_functions.R
@@ -17,3 +17,21 @@ beeCross = function() {}
 splitColony = function() {}
 
 # setPheno = function() {} # keep this one commented for now, we need some object-oriented magic for this to work on our colony and not to clash with AlphaSimR:::setPheno()
+
+
+
+#Things to add in the future/ check list 
+#genetic model --> they have either infinitesimal or finite (we don't have to worry about this)
+               --> #simulate queen and worker effects (yet to do)
+               --> #simulate demographic history (in progress)
+#population --> specify the size of the population (#colonies/y)
+            --> #simulate passive population (not actively selected but exchanged queens/drones with the selected)
+            --> #specify the rates of exchange between selected and non-selected (we can control this within the breeding program
+            --> #specify min/max age of queen to produce queens/drones
+            --> #culling ages
+#mating --> specify how queen of the breding and passive populations mate - free mating, AI, breeding stations
+        --> #decide how many drones are involved
+        --> #For mating stations specify - drone producting queens on mating stations, whether they are related, and how secure is the mating station (probability of a drone in the DCa coming from the mating stations hives)
+#selection - rates of queen selection
+           - #how many sisters from the same sister group (within/across family selection)
+           - #perform: phenotypic, genotypic, random selection on selection based on EBVs
