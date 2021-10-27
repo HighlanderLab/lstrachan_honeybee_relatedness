bbaaf812fa40ae6aaafa9ba8b96d9ddf1f926416
janaobsteter <obsteter.jana@gmail.com>
2021-10-27 15:02:18 +0200

    Edited the global paramteres

---
+++ GlobalParameters.R
@@ -0,0 +1,28 @@
+Global parameters----
+
+ ## -- Simulating the genome and the founder population -- ##
+nFounders = 
+nChromosomes =
+nSegSites = 
+
+## -- Parameters of the breeding scheme --##
+nColonies = #number of colonies 
+nWorkersPerColony = #number of bees per colony 
+nDronesPerColony = 
+
+pSwarm = #percentage of colony to swarm 
+pSupersede = #probability of supersedure
+pWinterLosses = 
+avgAgeQueen = 
+
+## -- Mating parameters --##
+nFathers = 
+nColoniesPerDCA = 
+  
+
+## -- Parameters for simulating the phenotype -- ##
+varDir = 
+varMat = 
+covDirMat = 
+varE = 
+
