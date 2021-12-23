298613f353a6855e366e2e7ce7928a924b203792
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2021-12-23 16:08:59 +0000

    YearCycle.R with devel SIMplyBee

--- YearCycle.R
+++ YearCycle.R
@@ -12,7 +12,7 @@ library(R6)
 # Laura
 #AlphaSimRBeeFolder <- "~/Desktop/GitHub/Fork/AlphaSimRBee"
 # Gregor
-#AlphaSimRBeeFolder <- "~/Documents/5_Storages/GitBox/AlphaSimRBee/AlphaSimRBee" 
+#AlphaSimRBeeFolder <- "~/Documents/5_Storages/GitBox/AlphaSimRBee/AlphaSimRBee"
 # Jernej
 AlphaSimRBeeFolder <- "C:/Users/jernejb/Desktop/git/AlphaSimRBee/SIMplyBee"
 
@@ -35,6 +35,7 @@ nDronesFull <- nWorkersFull * 0.2
 if (nDronesFull < (nAvgFathers * 2)) {
   nDronesFull <-nAvgFathers * 2
 }
+nVirginQueensFull <- 30
 
 # Period1
 p1swarm <- 0.05
@@ -55,6 +56,7 @@ p3collapseAge1 <- 0.3
 loopTime <- data.frame(Rep = NA, tic = NA, toc = NA, msg = NA, time = NA)
 noQueens <- data.frame(Rep = NA, Age0 = NA, Age1 = NA, sum = NA)
 csdVariability <- data.frame(Rep = NA, year = NA, avgCSDage0 = NA )
+
 # Rep-loop ---------------------------------------------------------------------
 
 nRep <- 1
@@ -66,27 +68,25 @@ for (Rep in 1:nRep) {
 
   # Founder population ---------------------------------------------------------
 
-  founderGenomes <- quickHaplo(nInd = 1000,
+  founderGenomes <- quickHaplo(nInd = apiarySize * 2,
                                nChr = 16,
                                segSites = 1000)
-  SP <- SimParamBee$new(founderGenomes, csdChr = 4, nCsdAlleles = 128)
+  SP <- SimParamBee$new(founderGenomes, csdChr = 3, nCsdAlleles = 128)
+  SP <- SimParamBee$new(founderGenomes, csdChr = NULL, nCsdAlleles = 128)
+  SP$nWorkers <- nWorkersFull
+  SP$nDrones <- nDronesFull
+  SP$nVirginQueens <- nVirginQueensFull
   base <- newPop(founderGenomes)
 
   # Year-loop ------------------------------------------------------------------
 
-  nYear <- 1
+  nYear <- 20
   for (year in 1:nYear) {
     # year <- 1
     # year <- year + 1
     cat(paste0("Year: ", year, "/", nYear, "\n"))
     if (year == 1) {
-
-      #age1 <- createColonies(pop = selectInd(base, nCol = apiarySize * 2, use = "rand"),
-                            # n = apiarySize, nAvgFathers = nAvgFathers)
-      age1 <- createColonies(pop = base, nCol = apiarySize, mated = TRUE,
-                                         nAvgFathers = 15, nDronesPerQueen = 100,
-                                         simParamBee = SP)
-
+      age1 <- createColonies(pop = base, nCol = apiarySize)
     } else {
       age2 <- age1
       age1 <- age0
@@ -96,70 +96,65 @@ for (Rep in 1:nRep) {
     }
 
     # Period1 ------------------------------------------------------------------
+    cat("Period1 -----------------------------------------------------------\n")
 
-    # Build-up the colonies
-    age1 <- buildUpColonies(age1, nWorkers = nWorkersFull, nDrones = nDronesFull)
+    cat("# Build-up the colonies\n")
+    age1 <- buildUpColonies(age1)
     if (year > 1) {
-      age2 <- buildUpColonies(age2, nWorkers = nWorkersFull, nDrones = nDronesFull)
+      age2 <- buildUpColonies(age2)
     }
 
-    # Split all age1 colonies
-    tmp <- splitColonies(age1)
-    age1 <- tmp$remnants
-    # The queens of the splits are 0 years old
-    age0p1 <- tmp$splits
+    cat("# Split all age1 colonies\n")
+    tmpS1 <- splitColonies(age1)
+    age1 <- tmpS1$remnants
+    age0p1 <- tmpS1$splits # The queens of the splits are 0 years old
 
     if (year > 1) {
-      # Split all age2 colonies
-      tmp <- splitColonies(age2)
-      age2 <- tmp$remnants
-      # The queens of the splits are 0 years old
-      age0p1 <- c(age0p1, tmp$splits)
+      cat("# Split all age2 colonies\n")
+      tmpS2 <- splitColonies(age2)
+      age2 <- tmpS2$remnants
+      age0p1 <- c(age0p1, tmpS2$splits) # The queens of the splits are 0 years old
     }
 
-    # Create virgin queens
+    cat("# Create virgin queens\n")
     # Sample colony for the virgin queens
-    # TODO: this is likely an inbreeding bottleneck - just one colony producing all virgin queens!
-    #       maybe we should use selectColonies() and then create virginQueens from them, but we
-    #       need createVirginQueens() to work with multiple colonies too, and equally for createWorkers()
-    #       and createDrones()
-    virginDonor <- sample.int(n = nColonies(age1), size = 1)
-    virginQueens <- createVirginQueens(age1[[virginDonor]], nInd = nColonies(age0p1) * 10)
+    virginDonor <- selectColonies(age1, n = 1)
+    virginQueens <- getVirginQueens(virginDonor[[1]])
 
-    # Requeen the splits --> queens are now 0 years old
-    age0p1 <- reQueenColonies(age0p1, queens = virginQueens$virgin_queens)
+    cat("# Requeen the splits\n")
+    age0p1 <- reQueenColonies(age0p1, queens = virginQueens) # queens are now 0 years old
 
-    # Swarm a percentage of age1 colonies
-    tmp <- pullColonies(age1, p = p1swarm)
-    age1 <- tmp$remainingColonies
-    tmp <- swarmColonies(tmp$pulledColonies)
-    age0p1 <- c(age0p1, tmp$remnants)
-    age1 <- c(age1, tmp$swarms)
+    cat("# Swarm a percentage of age1 colonies\n")
+    tmpPa <- pullColonies(age1, p = p1swarm)
+    age1 <- tmpPa$remainingColonies
+    tmpW1 <- swarmColonies(tmpPa$pulledColonies)
+    age0p1 <- c(age0p1, tmpW1$remnants)
+    age1 <- c(age1, tmpW1$swarms)
 
     if (year > 1) {
-      # Swarm a percentage of age2 colonies
-      tmp <- pullColonies(age2, p = p1swarm)
-      age2 <- tmp$remainingColonies
-      tmp <- swarmColonies(tmp$pulledColonies)
-      age0p1 <- c(age0p1, tmp$remnants)
-      age2 <- c(age2, tmp$swarms)
+      cat("# Swarm a percentage of age2 colonies\n")
+      tmpPb <- pullColonies(age2, p = p1swarm)
+      age2 <- tmpPb$remainingColonies
+      tmpW2 <- swarmColonies(tmpPb$pulledColonies)
+      age0p1 <- c(age0p1, tmpW2$remnants)
+      age2 <- c(age2, tmpW2$swarms)
     }
 
-    # Supersede age1 colonies
-    tmp <- pullColonies(age1, p = p1supersede)
-    age1 <- tmp$remainingColonies
-    tmp <- supersedeColonies(tmp$pulledColonies)
-    age0p1 <- c(age0p1, tmp)
+    cat("# Supersede age1 colonies\n")
+    tmpPc <- pullColonies(age1, p = p1supersede)
+    age1 <- tmpPc$remainingColonies
+    tmpQ1 <- supersedeColonies(tmpPc$pulledColonies)
+    age0p1 <- c(age0p1, tmpQ1)
 
     if (year > 1) {
-      # Supersede age2 colonies
-      tmp <- pullColonies(age2, p = p1supersede)
-      age2 <- tmp$remainingColonies
-      tmp <- supersedeColonies(tmp$pulledColonies)
-      age0p1 <- c(age0p1, tmp)
+      cat("# Supersede age2 colonies\n")
+      tmpPd <- pullColonies(age2, p = p1supersede)
+      age2 <- tmpPd$remainingColonies
+      tmpQ2 <- supersedeColonies(tmpPd$pulledColonies)
+      age0p1 <- c(age0p1, tmpQ2)
     }
 
-    # Mate the split colonies
+    cat("# Mate the split colonies\n")
     if (year == 1) {
       DCA <- createDCA(age1, nInd = nDronesFull)
     } else {
@@ -167,90 +162,89 @@ for (Rep in 1:nRep) {
     }
     age0p1 <- crossColonies(age0p1, DCA = DCA, nAvgFathers = nAvgFathers)
 
-    # Collapse
+    cat("# Collapse collonies\n")
     age1 <- selectColonies(age1, p = 1 - p1collapse)
     if (year > 1) {
       age2 <- selectColonies(age2, p = 1 - p1collapse)
     }
 
     # Period2 ------------------------------------------------------------------
+    cat("Period2 -----------------------------------------------------------\n")
 
-    # Swarm a percentage of age1 colonies
-    tmp <- pullColonies(age1, p = p2swarm)
-    age1 <- tmp$remainingColonies
-    tmp <- swarmColonies(tmp$pulledColonies)
-    # The queens of the remnant colonies are of age 0
-    age0p2 <- tmp$remnants
-    age1 <- c(age1, tmp$swarms)
-
-    if (year > 1) {
-      # Swarm a percentage of age2 colonies
-      tmp <- pullColonies(age2, p = p2swarm)
-      age2 <- tmp$remainingColonies
-      tmp <- swarmColonies(tmp$pulledColonies)
-      # The queens of the remnant colonies are of age 0
-      age0p2 <- c(age0p2, tmp$remnants)
-      age2 <- c(age2, tmp$swarms)
-    }
-
-    # Supersede a part of age1 colonies
-    tmp <- pullColonies(age1, p = p2supersede)
-    age1 <- tmp$remainingColonies
-    tmp <- supersedeColonies(tmp$pulledColonies)
-    # The queens of superseded colonies are of age 0
-    age0p2 <- c(age0p2, tmp)
+    cat("# Swarm a percentage of age1 colonies\n")
+    tmpPa <- pullColonies(age1, p = p2swarm)
+    age1 <- tmpPa$remainingColonies
+    tmpW1 <- swarmColonies(tmpPa$pulledColonies)
+    age0p2 <- tmpW1$remnants # The queens of the remnant colonies are of age 0
+    age1 <- c(age1, tmpW1$swarms)
 
     if (year > 1) {
-      # Supersede a part of age1 colonies
-      tmp <- pullColonies(age2, p = p2supersede)
-      age2 <- tmp$remainingColonies
-      tmp <- supersedeColonies(tmp$pulledColonies)
-      # The queens of superseded colonies are of age 0
-      age0p2 <- c(age0p2, tmp)
+      cat("# Swarm a percentage of age2 colonies\n")
+      tmpPb <- pullColonies(age2, p = p2swarm)
+      age2 <- tmpPb$remainingColonies
+      tmpW2 <- swarmColonies(tmpPb$pulledColonies)
+      age0p2 <- c(age0p2, tmpW2$remnants) # The queens of the remnant colonies are of age 0
+      age2 <- c(age2, tmpW2$swarms)
     }
 
-    # Replace all the drones
+    cat("# Supersede a part of age1 colonies\n")
+    tmpPc <- pullColonies(age1, p = p2supersede)
+    age1 <- tmpPc$remainingColonies
+    tmpQ1 <- supersedeColonies(tmpPc$pulledColonies)
+    age0p2 <- c(age0p2, tmpQ1) # The queens of superseded colonies are of age 0
+
+    if (year > 1) {
+      cat("# Supersede a part of age2 colonies\n")
+      tmpPd <- pullColonies(age2, p = p2supersede)
+      age2 <- tmpPd$remainingColonies
+      tmpQ2 <- supersedeColonies(tmpPd$pulledColonies)
+      age0p2 <- c(age0p2, tmpQ2) # The queens of superseded colonies are of age 0
+    }
+
+    cat("# Replace all the drones\n")
     age1 <- replaceDronesColonies(age1)
     if (year > 1) {
       age2 <- replaceDronesColonies(age2)
     }
 
-    # Mate the colonies
+    cat("# Mate age 0 period 2 swarms and splits\n")
     if (year == 1) {
       DCA <- createDCA(age1, nInd = nDronesFull)
     } else {
       DCA <- createDCA(c(age1, age2), nInd = nDronesFull)
     }
-    # Cross age 0 period 2 swarms and splits
     age0p2 <- crossColonies(age0p2, DCA = DCA, nAvgFathers = nAvgFathers)
 
-    # Collapse
+    cat("# Collapse collonies\n")
     age1 <- selectColonies(age1, p = 1 - p2collapse)
     if (year > 1) {
       age2 <- selectColonies(age2, p = 1 - p2collapse)
     }
 
-    # Merge all age 0 colonies (from both periods)
+    cat("# Merge all age 0 colonies (from both periods)\n")
     age0 <- c(age0p1, age0p2)
 
     # Period3 ------------------------------------------------------------------
+    cat("Period3 -----------------------------------------------------------\n")
 
-    # Collapse age0 queens
+    cat("# Collapse age0 queens\n")
     age0 <- selectColonies(age0, p = (1 - p3collapseAge0))
     age1 <- selectColonies(age1, p = (1 - p3collapseAge1))
-    age2 <- NULL #We don't need this but just to show the workflow!!!
-    
-    # Get the intracolonial csd variability-------------------------------------
-    q <- nCsdAlleles(age0)
-    nCsdAll <- c()
-    for (n in 1:nColonies(age0)){
-      w <- q[[n]]$workers
-      nCsdAll <- c(nCsdAll, w)
+    age2 <- NULL # We don't need this but just to show the workflow!!!
+
+    # Get the intra-colonial csd variability -----------------------------------
+    cat("Get the intra-colonial csd variability ----------------------------\n")
+
+    if (isCsdActive()) {
+      tmp <- nCsdAlleles(age0)
+      # TODO: we need collapse here!
+      nCsdAll <- sapply(X = tmp, FUN = function(z) z$workers)
+      nCSD <- sum(nCsdAll) / nColonies(age0)
+      csdVariability <- rbind(csdVariability, c(Rep, year, nCSD))
     }
-    nCSD <- sum(nCsdAll)/nColonies(age0)
-    csdVariability <- rbind(csdVariability, c(Rep, year, nCSD))
 
     # Maintain the number of colonies ------------------------------------------
+    cat("Maintain the number of colonies -----------------------------------\n")
 
     # keep all of age1, age0 swarmed so we build it up with some splits, while we remove (sell) the other splits
     if ((nColonies(age0) + nColonies(age1)) > apiarySize) { # check if the sum of all colonies is greater than apiary size
@@ -278,6 +272,11 @@ for (Rep in 1:nRep) {
   noQueens <- rbind(noQueens, c(Rep, nColonies(age0), nColonies(age1), (nColonies(age0) + nColonies(age1))))
   a <- toc()
   loopTime <- rbind(loopTime, c(Rep, a$tic, a$toc, a$msg, (a$toc - a$tic)))
+
+  nVirginQueens(age2)
+  nVirginQueens(age1)
+  nVirginQueens(age0p1)
+
 } # Rep-loop
 
 ggplot(noQueens, aes(Rep, sum)) +
